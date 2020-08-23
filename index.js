448a4d47505930371b8c68fbad57e21a50657da4
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-08-23 15:33:18 -0700

    added fatch

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function getMasks(color='', keyword='', price, material='') {
   const path = `/v2/listings/active?limit=20&keywords=face%20mask&tags=${keyword},${material},${color}&max_price=${price}&includes=Images`;
   const api_key = "&api_key=l3a7mjohsu0hq3fbwq6p7pd3"
   const fetchurl = `${proxyurl}${url}${path}${api_key}`
+
+  fetch(fetchurl)
+    .then(response => response.json())
+    .then(responseJson => {
+      displayMasks(responseJson)
+    })
+}
+
+function showLoading() {
+  let img = `<img width="50%" style="margin:0 auto;display:block;" src="https://cdn.lowgif.com/full/c57c4d0d4333569d-index-of-skin-frontend-sns-amoda-default-images.gif" />`
+  $("#items-container").html(img);
+}
+
